1fff11a7324d90736a241d975d469983cf2167e1
Hamza <ohamza762@gmail.com>
2020-07-10 15:35:18 +0100

    First Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+export class About extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>About this page</h1>
+        <p>App to search Github users</p>
+        <p>Version: 1.0.0</p>
+      </Fragment>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/repos/Repoitem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Repoitem = ({ repo }) => {
+  return (
+    <div className='card'>
+      <h3>
+        <a href={repo.html_url}>{repo.name}</a>
+      </h3>
+    </div>
+  );
+};
+
+Repoitem.prototypes = {
+  repo: PropTypes.object.isRequired,
+};
+
+export default Repoitem;
